24944b1a03dc314726fcfc59a24c6633a0adb63a
Damián Silvani <munshkr@gmail.com>
2019-04-22 19:55:17 -0300

    Remove unneeded methods for SuperDirt

--- lib/xi/superdirt/stream.rb
+++ lib/xi/superdirt/stream.rb
@@ -6,50 +6,19 @@ module Xi::Superdirt
   class Stream < Xi::Stream
     include Xi::OSC
 
-    MAX_NODE_ID = 10000
-    DEFAULT_PARAMS = {
-      out:  0,
-      amp:  1.0,
-      pan:  0.0,
-      vel:  127,
-    }
-
     def initialize(name, clock, server: "localhost", port: 57120, base_node_id: 2000, **opts)
       debug "Xi::Superdirt initialized"
       super
-
-      @base_node_id = base_node_id
-      @playing_synths = [].to_set
-      at_exit { free_playing_synths }
     end
 
     def set(params)
-      super(gate: params[:gate] || :freq, **params)
-    end
-
-    def stop
-      @mutex.synchronize do
-        @playing_synths.each do |so_id|
-          n_set(node_id(so_id), gate: 0)
-        end
-      end
-      super
-    end
-
-    def free_playing_synths
-      n_free(*@playing_synths.map { |so_id| node_id(so_id) })
-    end
-
-    def node_id(so_id)
-      (@base_node_id + so_id) % MAX_NODE_ID
+      super(gate: params[:gate] || :s, **params)
     end
 
     private
 
     def transform_state
       super
-
-      @state = DEFAULT_PARAMS.merge(@state)
 
       if changed_param?(:db) && !changed_param?(:amp)
         @state[:amp] = @state[:db].db_to_amp
@@ -60,13 +29,16 @@ module Xi::Superdirt
         @state[:freq] = Array(@state[:midinote]).map(&:midi_to_cps)
         @changed_params << :freq
       end
+
+      @state[:s] ||= @name.to_s
     end
 
     def do_gate_on_change(changes)
       debug "Gate on change: #{changes}"
 
-      name = @state[:s] || :default
-      state_params = @state.reject { |k, _| %i(s).include?(k) }
+      name = @state[:s]
+      state_params = @state.reject { |k, _| %i(gate).include?(k) }
+      debug(@state)
 
       freq = Array(state_params[:freq])
 
@@ -76,45 +48,18 @@ module Xi::Superdirt
         change.fetch(:so_ids).each.with_index do |so_id, i|
           freq_i = freq.size > 0 ? freq[i % freq.size] : nil
 
-          s_new(name, node_id(so_id), **state_params, freq: freq_i, at: at)
-          @playing_synths << so_id
+          send_play2(name, **state_params, freq: freq_i, at: at)
         end
       end
     end
 
-    def do_gate_off_change(changes)
-      debug "Gate off change: #{changes}"
-
-      changes.each do |change|
-        at = Time.at(change.fetch(:at))
-
-        change.fetch(:so_ids).each do |so_id|
-          n_set(node_id(so_id), gate: 0, at: at)
-          @playing_synths.delete(so_id)
-        end
-      end
-    end
-
-    def do_state_change
-      debug "State change: #{changed_state}"
-      @playing_synths.each do |so_id|
-        n_set(node_id(so_id), **changed_state)
-      end
-    end
-
-    def n_set(id, at: Time.now, **args)
-      #debug "n_set: args #{args}"
-      send_bundle("/play2", "s", *osc_args(args), at: at)
-    end
-
-    def s_new(name, id, add_action: 0, target_id: 1, at: Time.now, **args)
-      debug "s_new: args #{args}"
-      send_bundle("/play2", "s", name.to_s, id.to_i, add_action.to_i,
-                  target_id.to_i, *osc_args(args), at: at)
-    end
-
-    def n_free(*ids, at: Time.now)
-      send_bundle("/n_free", *ids, at: at)
+    def send_play2(name, at: Time.now, **args)
+      debug "osc_play2: args #{args}"
+      send_bundle("/play2",
+                  "cps", @clock.cps.to_f,
+                  "cycle", @clock.current_cycle.to_f,
+                  "delta", 0.22,
+                  *osc_args(args), at: at)
     end
 
     def osc_args(**args)
@@ -124,7 +69,8 @@ module Xi::Superdirt
     def coerce_osc_value(value)
       v = Array(value).first
       v = v.to_f if v.is_a?(Rational)
-      v = v.to_i if !v.is_a?(Float) && !v.is_a?(String) && !v.is_a?(Symbol)
+      v = v.to_s if v.is_a?(Symbol)
+      v = v.to_i if !v.is_a?(Float) && !v.is_a?(String)
       v
     end
   end
